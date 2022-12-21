126df97400a53b7ca62742a259dc719c1118a232
Vadim <NSPMSouL16@gmail.com>
2022-12-21 19:02:31 +0300

    header add

---
+++ src/Components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import { Avatar, Container, Button } from '@mui/material';
+import styles from '/styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <Container className={styles.content} maxWidth={false}>
+        <Avatar sx={{ width: 60, height: 60 }} src="/static/images/logo.svg" alt="logo" />
+        <div>
+          <Link href="/signin">
+            <Button color="primary" className={styles.button} variant="outlined">
+              Sign in
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button color="primary" className={styles.button} variant="outlined">
+              Sign up
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
--- src/Components/Vadya.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Vadya = () => {
-  return <div>Vadya</div>;
-};
-
-export default Vadya;
