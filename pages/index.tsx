a713a9357c792bcf952e12c0ece528b7f01ec83f
ArtisTTT <artemis09035@gmail.com>
2022-12-22 12:49:14 +0800

    exlint

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,25 @@ import styles from '../styles/MainMage.module.css';
 import { Button, TextField } from '@mui/material';
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.content}>
-        <div className={styles.name}>Friendly Carrier</div>
-        <div className={styles.description}>
-          We care about our clients.<br/> Reliability is our most important advantage
-        </div>
-        <div className={styles.email}>
-        <TextField id="email" label="Email" variant="standard" />
-          <Button color="primary" className={styles.button} variant="contained">
-            Register Now
-          </Button>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.content}>
+                <div className={styles.name}>Friendly Carrier</div>
+                <div className={styles.description}>
+                    We care about our clients.
+                    <br /> Reliability is our most important advantage
+                </div>
+                <div className={styles.email}>
+                    <TextField id='email' label='Email' variant='standard' />
+                    <Button
+                        color='primary'
+                        className={styles.button}
+                        variant='contained'
+                    >
+                        Register Now
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
 }
