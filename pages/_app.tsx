04b3261898d37dac50eab0ea1efb66308a50db65
ArtisTTT <artemis09035@gmail.com>
2022-12-22 13:41:45 +0800

    logo fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../src/Components/Layout';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../src/helpers/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeProvider>
+    );
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,25 +4,37 @@ import { Avatar, Container, Button } from '@mui/material';
 import styles from '/styles/Header.module.css';
 
 const Header = () => {
-  return (
-    <header className={styles.wrapper}>
-      <Container className={styles.content} maxWidth={false}>
-        <Avatar sx={{ width: 60, height: 60 }} src="/static/images/logo.svg" alt="logo" />
-        <div>
-          <Link href="/signin">
-            <Button color="primary" className={styles.button} variant="outlined">
-              Sign in
-            </Button>
-          </Link>
-          <Link href="/signup">
-            <Button color="primary" className={styles.button} variant="outlined">
-              Sign up
-            </Button>
-          </Link>
-        </div>
-      </Container>
-    </header>
-  );
+    return (
+        <header className={styles.wrapper}>
+            <Container className={styles.content} maxWidth={false}>
+                <Avatar
+                    sx={{ width: 60, height: 60 }}
+                    src='/static/images/pre-logo.png'
+                    alt='logo'
+                />
+                <div>
+                    <Link href='/signin'>
+                        <Button
+                            color='primary'
+                            className={styles.button}
+                            variant='outlined'
+                        >
+                            Sign in
+                        </Button>
+                    </Link>
+                    <Link href='/signup'>
+                        <Button
+                            color='primary'
+                            className={styles.button}
+                            variant='outlined'
+                        >
+                            Sign up
+                        </Button>
+                    </Link>
+                </div>
+            </Container>
+        </header>
+    );
 };
 
 export default Header;
