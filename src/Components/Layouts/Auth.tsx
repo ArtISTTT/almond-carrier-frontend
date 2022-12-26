9cebf9a3c9d71d7fb8c69dddedf03b28c80529f3
ArtisTTT <artemis09035@gmail.com>
2022-12-26 13:47:14 +0800

    get user fix

--- src/Components/Layouts/Auth.tsx
+++ src/Components/Layouts/Auth.tsx
@@ -2,13 +2,17 @@ import { Avatar } from '@mui/material';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { getCurrentUser } from '../../api/auth';
+import { useAppDispatch } from '../../redux/hooks';
+import { addUserData } from '../../redux/slices/userSlice';
 
 const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const router = useRouter();
+    const dispath = useAppDispatch();
 
     useEffect(() => {
         getCurrentUser().then(data => {
-            if (data.ok) {
+            if (data.ok && data.user) {
+                dispath(addUserData(data.user));
                 router.push('/carrier');
             } else {
                 router.push('/');
