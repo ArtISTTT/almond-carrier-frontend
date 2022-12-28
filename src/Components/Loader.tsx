d42e7ae163561ea38fbaee3f9898efeda6263649
ArtisTTT <artemis09035@gmail.com>
2022-12-28 21:38:10 +0800

    loader changed

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -3,8 +3,16 @@ import styles from '../../styles/Loader.module.css';
 
 const Loader = () => {
     return (
-        <div className={styles.loader}>
-            <div className={styles.dotElastic}></div>
+        <div className={styles.loaderContainer}>
+            <div className={styles.logoWrapper}>
+                <div className={styles.loaderWrapper}>
+                    <div className={styles.logo} />
+                    <div className={styles.loader}>
+                        <span />
+                    </div>
+                </div>
+                <span>Friendly carrier</span>
+            </div>
         </div>
     );
 };
