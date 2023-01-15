ee1ec8074776c06cb73f7904c49ecbc20d94b1f7
ArtisTTT <artemis09035@gmail.com>
2023-01-15 18:58:43 +0800

    fix 123

--- src/Components/MyProfile/ProfileOrders.tsx
+++ src/Components/MyProfile/ProfileOrders.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import styles from '../../../styles/ProfileOrders.module.css';
 import { Pagination, Typography } from '@mui/material';
-import OrderItem from 'src/Components/Orders/OrderItem';
+import OrderItem from 'src/Components/OrderComponents/OrderItem';
 import { useSelector } from 'react-redux';
 import { selectMyOrders } from '../../redux/selectors/orders';
 import { useLoadOwnOrders } from '../../redux/hooks/useLoadOwnOrders';
