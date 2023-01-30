acb8518f52b77e0e28b054271f5e44da3bc39c77
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-01-30 12:09:42 +0300

    welcomePage fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -247,7 +247,7 @@ export default function Welcome() {
                                 src='/static/images/main-page/advantage-one.png'
                                 alt=''
                             />
-                            <div className={styles.advantageText}>
+                            <div className={cn(styles.advantageText, styles.advantageTextAnotherColor)}>
                                 <Typography
                                     variant='h3'
                                     component='h3'
