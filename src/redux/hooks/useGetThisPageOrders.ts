beef0d91721401f5f51b52705e58db0120a60576
Vadim <NSPMSouL16@gmail.com>
2023-02-24 19:04:40 +0300

    fix hook2

--- src/redux/hooks/useGetThisPageOrders.ts
+++ src/redux/hooks/useGetThisPageOrders.ts
@@ -7,8 +7,6 @@ interface IProps {
 }
 
 export const useGetThisPageOrders = ({ orders, page }: IProps) => {
-    
     const [start, end] = React.useMemo(() => [page * 4 - 4, page * 4], [page]);
-
     return orders.slice(start, end) as IOrder[];
 };
