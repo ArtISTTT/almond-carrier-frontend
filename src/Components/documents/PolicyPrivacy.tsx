dcc0d575a4046946e2f73aa70dbb2847c09156e9
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-02-26 16:01:16 +0300

    terms and conditions

---
+++ src/Components/documents/PolicyPrivacy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const PolicyPrivacy: React.FC = () => {
+    return (
+        <div>
+        ajdhgasdlj dskjah
+        </div>
+    )
+}
+
+export default PolicyPrivacy;
