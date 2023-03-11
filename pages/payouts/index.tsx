77621c82cfc1d0e87d0c44794f151ac23069beef
Vadim <NSPMSouL16@gmail.com>
2023-03-11 19:29:54 +0300

    fix P

--- pages/payouts/index.tsx
+++ pages/payouts/index.tsx
@@ -1,6 +1,6 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
-import Payouts from 'src/Components/payouts/Payouts';
+import Payouts from 'src/Components/Payouts/Payouts';
 import PrivateLayout from '../../src/Components/Layouts/Private';
 import UserLayout from '../../src/Components/Layouts/User';
 import { privateTypes } from '../../src/interfaces/private';
